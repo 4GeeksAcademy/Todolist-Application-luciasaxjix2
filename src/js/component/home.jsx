6a87df103b4f7c1b22f476d58d719f710d9626f4
luciasaxjix2 <128443618+luciasaxjix2@users.noreply.github.com>
2023-08-01 02:34:03 +0000

    Avance

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,69 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+ const [tarea, setTarea] = useState("")
+ const [lista, setLista] = useState(["lavar el carro", "ir al cine"])
+
+ const handleInput = (e) => {
+  let texto = e.target.value
+  if (e.keyCode == 13) {
+   setTarea(texto)
+   //Una primera aproximación para agregar a la lista es usando una variable auxiliar
+   //let tempArr = lista.slice() //copia de arreglo por valor
+   //tempArr.push(texto)
+   //setLista(tempArr)
+
+   //Una segunda aproximación es usando el operador spread ...
+   setLista([...lista, texto])
+  }
+ }
+
+ const deleteTask = (index) => { 
+  let tempArr = lista.slice() //copiar el estado lista en una variable auxiliar
+  tempArr = tempArr.filter((item, index2) => { return index2 != index })
+  setLista(tempArr)
+ }
+
+ return (
+  <>
+	<h1>Todos</h1>
+		<div class="paper">
+				<div className="text-center">
+					<input placeholder="Escriba aquí una tarea"
+					onKeyUp={
+					(e) => { handleInput(e) }
+					} />
+				</div>
+				<div>
+					Tarea es: {tarea}
+				</div>
+			<div>
+				La lista de tareas es:
+				<ul>
+				{
+				lista && lista.length > 0 ?
+				<>{
+					lista.map((item, index) => {
+					return <li key={index}>
+					{item}
+					<button type="button" onClick={e => { deleteTask(index) }}>
+					❌
+					</button>
+					</li>
+					})
+				}</>
+				: "la lista está vacía"
+				}
+				</ul>
+			</div>
 		</div>
-	);
+  </>
+ );
 };
 
 export default Home;
